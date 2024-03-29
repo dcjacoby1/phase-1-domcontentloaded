3b343dcb6e2c94bff871e5e075575cd4dec18efc
Dan Jacoby <jacobydan01@gmail.com>
2024-03-29 12:34:04 -0400

    domcont

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+//arrow format
+//adds event listener for page loading
+document.addEventListener("DOMContentLoaded", () => {
+    //grabs text field and changes the content
+    document.querySelector('#text').textContent = "This is really cool!"
+  })
+  //could be done with defer in html tag for JS <script>
+  //<script src="index.js" defer></script>
+
+
+// function format:
+//   document.addEventListener("DOMContentLoaded", function() {
+//     document.querySelector('#text').textContent = "This is really cool!"
+//   })
